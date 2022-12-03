f8c32f1c51e2010269548268532fb46071389dcc
Edgar Diaz <ediaz@ec.krugercorp.com>
2022-12-03 16:38:22 -0500

    Servidor express, servicios web con node.

---
+++ index.ts
@@ -0,0 +1,22 @@
+import Server from "./classes/server";
+import { SERVER_PORT } from "./global/environment";
+import router from "./routes/router";
+import bodyParser from "body-parser";
+import cors from "cors";
+const nombre = 'Edgar Efraín Diaz Zhinin';
+console.log(`Mi nombre es: ${nombre}`);
+
+const server = new Server();
+
+//BODYPARSE
+server.app.use(bodyParser.urlencoded({ extended: true }));
+server.app.use(bodyParser.json());
+
+
+// CORS
+server.app.use(cors({ origin: true, credentials: true }));
+
+// RUTAS DE SERVICIO
+server.app.use('/', router);
+
+server.start(() => { console.log(`Servidor corriendo en el servidor: ${SERVER_PORT} `) });
